550a4d27883ea51a642f5aa63b7174a661aca43a
grouting <105883378+grouting@users.noreply.github.com>
2024-09-10 13:14:30 +0100

    fully qualifying

--- src/lib.rs
+++ src/lib.rs
@@ -3,28 +3,28 @@ use quote::ToTokens;
 use syn::{parse::Parse, parse_macro_input, Expr, LitInt, Token, Type};
 
 struct Confused {
-    number: u8,
-    ok: Type,
-    err: Option<Type>,
+	number: u8,
+	ok: Type,
+	err: Option<Type>,
 }
 
 impl Parse for Confused {
-    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
-        let number_literal: LitInt = input.parse()?;
-        let number = number_literal.base10_parse::<u8>()?;
+	fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+		let number_literal: LitInt = input.parse()?;
+		let number = number_literal.base10_parse::<u8>()?;
 
-        input.parse::<Token![,]>()?;
+		input.parse::<Token![,]>()?;
 
-        let ok: Type = input.parse()?;
+		let ok: Type = input.parse()?;
 
-        let err = if input.parse::<Token![,]>().is_ok() {
-            Some(input.parse::<Type>()?)
-        } else {
-            None
-        };
+		let err = if input.parse::<Token![,]>().is_ok() {
+			Some(input.parse::<Type>()?)
+		} else {
+			None
+		};
 
-        Ok(Self { number, ok, err })
-    }
+		Ok(Self { number, ok, err })
+	}
 }
 
 /// # Usage
@@ -34,43 +34,43 @@ impl Parse for Confused {
 /// ```
 #[proc_macro]
 pub fn confused(input: TokenStream) -> TokenStream {
-    let config = parse_macro_input!(input as Confused);
+	let config = parse_macro_input!(input as Confused);
 
-    let ok_type_name = config.ok.to_token_stream().to_string();
-    let err_type_name = config.err.map(|err| err.to_token_stream().to_string());
+	let ok_type_name = config.ok.to_token_stream().to_string();
+	let err_type_name = config.err.map(|err| err.to_token_stream().to_string());
 
-    let mut out = String::new();
+	let mut out = String::new();
 
-    for _ in 0..config.number {
-        out.push_str("Result<");
-    }
+	for _ in 0..config.number {
+		out.push_str("::core::result::Result<");
+	}
 
-    out.push_str(&ok_type_name);
+	out.push_str(&ok_type_name);
 
-    for _ in 0..config.number {
-        if let Some(ref err) = err_type_name {
-            out.push_str(&format!(", {err}>"));
-        } else {
-            out.push_str(", ()>");
-        }
-    }
+	for _ in 0..config.number {
+		if let Some(ref err) = err_type_name {
+			out.push_str(&format!(", {err}>"));
+		} else {
+			out.push_str(", ()>");
+		}
+	}
 
-    out.parse().unwrap()
+	out.parse().unwrap()
 }
 
 struct NestedOk(u8, Expr);
 
 impl Parse for NestedOk {
-    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
-        let number_literal: LitInt = input.parse()?;
-        let number = number_literal.base10_parse::<u8>()?;
+	fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+		let number_literal: LitInt = input.parse()?;
+		let number = number_literal.base10_parse::<u8>()?;
 
-        input.parse::<Token![,]>()?;
+		input.parse::<Token![,]>()?;
 
-        let expression: Expr = input.parse()?;
+		let expression: Expr = input.parse()?;
 
-        Ok(Self(number, expression))
-    }
+		Ok(Self(number, expression))
+	}
 }
 
 /// # Usage
@@ -79,19 +79,19 @@ impl Parse for NestedOk {
 /// ```
 #[proc_macro]
 pub fn confusion(input: TokenStream) -> TokenStream {
-    let number = parse_macro_input!(input as NestedOk);
+	let number = parse_macro_input!(input as NestedOk);
 
-    let mut out = String::new();
+	let mut out = String::new();
 
-    for _ in 0..number.0 {
-        out.push_str("Ok(");
-    }
+	for _ in 0..number.0 {
+		out.push_str("::core::result::Result::Ok(");
+	}
 
-    out.push_str(&number.1.into_token_stream().to_string());
+	out.push_str(&number.1.into_token_stream().to_string());
 
-    for _ in 0..number.0 {
-        out.push_str(")");
-    }
+	for _ in 0..number.0 {
+		out.push_str(")");
+	}
 
-    out.parse().unwrap()
+	out.parse().unwrap()
 }
